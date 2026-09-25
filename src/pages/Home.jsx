85c4b470fdf5012cef88ed7a936d4f4d81881364
user <lizosazi@gmail.com>
2026-09-25 16:45:58 +0200

    increased the spacing between navigation li elements"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './Home.css';
-import './Services.css';
+import './services.css';
 import BookingModal from '../components/BookingModal';
 import ServiceCard from '../components/ServiceCard';
 import {serviceCatalog} from '../data/serviceData';
